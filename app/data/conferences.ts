91bf2063b51e1a08d7ffa9efbe17ec701fb16f4e
Olav Nybø <olavnybo@gmail.com>
2016-01-23 15:32:16 +0100

    added more conferences

--- app/data/conferences.ts
+++ app/data/conferences.ts
@@ -34,14 +34,58 @@ const data = [
   },
   {
     id: 4,
-    title: "SmashingConf San Francisco",
-    start: new Date(2016, 4, 5),
-    end: new Date(2016, 4, 6),
-    venue: "Cowell Theater",
+    title: "SmashingConf Oxford",
+    start: new Date(2016, 3, 15),
+    end: new Date(2016, 3, 16),
+    venue: "Oxford Town Hall",
+    city: "Oxford",
+    country: "UK",
+    url: "http://smashingconf.com/",
+    description: "The conference will be packed with smart real-life solutions and techniques, ranging from front end to design to UX — and a few delightful surprises along the way. Two days, one track, 14 brilliant speakers and 350 fantastic attendees."
+  },
+  {
+    id: 5,
+    title: "Build 2016",
+    start: new Date(2016, 3, 30),
+    end: new Date(2016, 4, 1),
+    venue: "Moscone Center",
     city: "San Francisco",
     country: "USA",
-    url: "http://smashingconf.com/sf-2016/",
-    description: "With our very first SmashingConf San Francisco, we'll explore practical insights, tips, tricks and techniques that we all can apply to our work right away. 1 track, 2 conference days, 6 workshops, 16 excellent speakers and just 400 available seats."
+    url: "http://build.microsoft.com/",
+    description: "Microsoft continues to focus on enabling developers to do amazing work as businesses and industries transform in support of the shift to a cloud-first, mobile-first world. At Build 2016 we will present the latest tools and technologies and how they can help today’s developers be their most creative and productive. "
+  },
+  {
+    id: 6,
+    title: "ncrafts 2016",
+    start: new Date(2016, 5, 12),
+    end: new Date(2016, 5, 13),
+    venue: "?",
+    city: "Paris",
+    country: "France",
+    url: "http://ncrafts.io/",
+    description: "Professional Developer's conference"
+  },
+  {
+    id: 7,
+    title: "NDC Sydney",
+    start: new Date(2016, 8, 1),
+    end: new Date(2016, 8, 5),
+    venue: "Hilton Sydney",
+    city: "Sydney",
+    country: "Australia",
+    url: "http://ndcsydney.com/",
+    description: "NDC Sydney will have the same format as its European sisters NDC Oslo and NDC London with 2 days of workshops and 3 days of conference sessions"
+  },
+  {
+    id: 8,
+    title: "Trondheim Developer Conference",
+    start: new Date(2016, 10, 31),
+    end: new Date(2016, 10, 31),
+    venue: "Clarion Congress",
+    city: "Trondheim",
+    country: "Norway",
+    url: "http://2016.trondheimdc.no/",
+    description: "Norwegian Developer conference"
   }
 ];
 
